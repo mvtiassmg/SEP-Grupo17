079a7a6c53614f52320dd258d84556fcbe1081fe
mvtiassmg <msmunozg@uc.cl>
2025-08-24 19:00:05 -0400

    Agregando ClockDivider y Secuencia de LEDs

---
+++ Proyecto1/Proyecto1.srcs/sources_1/new/led_secuency.vhd
@@ -0,0 +1,32 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity led_secuency is
+    Port (
+        clk_in: in std_logic;
+        led   : out std_logic_vector(3 downto 0)
+    );
+end led_secuency;
+
+architecture Behavioral of led_secuency is
+    signal led_reg : std_logic_vector(3 downto 0) := "0000";
+begin
+    led <= led_reg;
+
+    process(clk_in)
+        variable counter: integer := 0;
+        variable led_on: integer := 0;
+    begin
+        if rising_edge(clk_in) then
+            counter := counter + 1;
+            if counter = 4 then
+                counter := 0;
+            end if;
+
+            led_on := 3 - counter;
+            led_reg <= (others => '0'); 
+            led_reg(led_on) <= '1';
+        end if;
+    end process;
+
+end Behavioral;
---
+++ Proyecto1/Proyecto1.srcs/sources_1/new/sequency_tester.vhd
@@ -0,0 +1,57 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity sequency_tester is
+    Port (
+        clk : in  std_logic; 
+        led        : out std_logic_vector(3 downto 0)
+    );
+end sequency_tester;
+
+architecture Behavioral of sequency_tester is
+
+    -- Señal para el reloj dividido de 2 Hz
+    signal clk_2Hz : std_logic;
+
+    -- Instancia del ClockDivider
+    component ClockDivider is
+        generic (
+            FREQ_IN  : integer := 125_000_000;
+            FREQ_OUT : integer := 10_000
+        );
+        Port (
+            clk_in  : in  std_logic;
+            clk_out : out std_logic
+        );
+    end component;
+
+    -- Instancia del LED Secuency
+    component led_secuency is
+        Port (
+            clk_in : in  std_logic;
+            led    : out std_logic_vector(3 downto 0)
+        );
+    end component;
+
+begin
+
+    -- ClockDivider: genera 2 Hz a partir de 125 MHz
+    u_clkdiv : ClockDivider
+        generic map (
+            FREQ_IN  => 125_000_000,
+            FREQ_OUT => 2       -- frecuencia deseada 2 Hz
+        )
+        port map (
+            clk_in  => clk,
+            clk_out => clk_2Hz
+        );
+
+    -- Secuencia de LEDs
+    u_ledseq : led_secuency
+        port map (
+            clk_in => clk_2Hz,
+            led    => led
+        );
+
+end Behavioral;
+
